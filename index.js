14c4b683ed94f5e6d338b4758f52935c9fb826e2
pablo128482 <ashley.c.clay@gmail.com>
2023-05-31 15:40:59 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+//Function Write Cards
+function writeCards(names, event) { 
+
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    thankYouMessages.push(message);
+  }
+  
+  return thankYouMessages;
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+  10
+  9
+  8
+  7
+  6
+  5
+  4
+  3
+  2
+  1
+  0
+}
+
+
